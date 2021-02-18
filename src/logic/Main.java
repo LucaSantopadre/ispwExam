aaa3a7f73f76f73c425c16a6e55fb6a2f574a6d3
Luca Santopadre <luca.santopadre92@gmail.com>
2021-02-18 09:50:25 +0100

    first commit

---
+++ src/logic/Main.java
@@ -0,0 +1,12 @@
+package logic;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logger.getGlobal().log(Level.INFO, "test");
+	}
+
+}
